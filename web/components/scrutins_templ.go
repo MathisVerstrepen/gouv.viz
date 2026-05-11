5c01697a395384cfb6a8828953b6ba5faecb3d56
Mathis Verstrepen <mathisverstrepen@gmail.com>
2026-05-11 18:55:10 -0400

    fix: update ScrutinCard to use correct ordinal indicator for scrutin number

--- web/components/scrutins_templ.go
+++ web/components/scrutins_templ.go
@@ -326,14 +326,14 @@ func ScrutinCard(scrutin ScrutinListItem) templ.Component {
 			templ_7745c5c3_Var14 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 24, "<article class=\"scrutin-card\"><div class=\"scrutin-card__meta\"><span>Scrutin n")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 24, "<article class=\"scrutin-card\"><div class=\"scrutin-card__meta\"><span>Scrutin n°")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var15 string
 		templ_7745c5c3_Var15, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(scrutin.Numero))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `scrutins.templ`, Line: 107, Col: 48}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `scrutins.templ`, Line: 107, Col: 50}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var15))
 		if templ_7745c5c3_Err != nil {
